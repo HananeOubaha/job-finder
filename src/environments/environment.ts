6cb6ca540500328de964f7970fc7a0f40573fe1b
Hanane Oubaha <Hananeouba03@gmail.com>
2026-02-09 15:30:00 +0100

    config: ajout des variables d environnement avec cles API Adzuna et URL du serveur JSON

---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  production: false,
+
+  // Configuration ADZUNA
+  adzunaAppId: '6239e870',
+  adzunaApiKey: '5dff94a3d86f0cae3252a859b00efd1b',
+  adzunaUrl: 'https://api.adzuna.com/v1/api/jobs',
+
+  // Configuration THE MUSE (Pas besoin de clé, juste l'URL)
+};
